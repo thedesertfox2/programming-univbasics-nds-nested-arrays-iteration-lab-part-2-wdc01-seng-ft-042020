8f0b746c2b4e3b734f2ef81cc535e997a62f3718
carefree-engine-5533 <alexander.gautherin@uconn.edu>
2020-04-13 15:27:25 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -5,7 +5,7 @@ def find_min_in_nested_arrays(src)
   row_index = 0
   while row_index < src.count do
     element_index = 0
-    array_min = [0]
+    array_min = 0
     while element_index < src[row_index].count do
       if src[row_index][element_index].min < array_min.count 
         p src[row_index][element_index]
@@ -13,6 +13,6 @@ def find_min_in_nested_arrays(src)
       element_index += 1
     end
    outer_results << array_min
-    row_index += 1
+   row_index += 1
   end
 end

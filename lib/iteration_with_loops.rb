33d452f720d248ac9b549357f74abd10f0a7f993
carefree-engine-5533 <alexander.gautherin@uconn.edu>
2020-04-13 15:04:27 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -13,5 +13,5 @@ def find_min_in_nested_arrays(src)
   row_index = 0
   while row_index < array_of_daily_temperatures.count do
     element_index = 0
-    min_in_nested
+    min_in_nested = array_of_daily_temperatures.min
 end
